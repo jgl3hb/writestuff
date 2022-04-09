f6ae93c4c1c538a217096d1fd4940555d8b76126
jgl3hb <lewis.jeffreyg@gmail.com>
2022-04-09 15:15:27 -0400

    add note model

--- myapp/models.py
+++ myapp/models.py
@@ -31,3 +31,18 @@ class User(db.Model, UserMixin):
     def __repr__(self):
         return f"Username {self.username}"
 
+class Note(db.Model):
+    __tablename__ = 'notes'
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    title = db.Column(db.String(140), nullable=False)
+    text = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+    def __init__(self, title, text, user_id):
+        self.title = title
+        self.text = text
+        self.user_id = user_id
+    
+    def __repr__(self):
+        return f"Note ID: {self.id} -- Date: {self.date} --- Title: {self.Title}"
