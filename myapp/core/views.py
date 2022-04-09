f1861a5f6ae6a4b4a72f25ff1fc6dfedfa2cd9e9
jgl3hb <lewis.jeffreyg@gmail.com>
2022-04-09 15:59:13 -0400

    core view update

--- myapp/core/views.py
+++ myapp/core/views.py
@@ -1,13 +1,13 @@
-# core/views.py 
-
 from flask import render_template, request, Blueprint
+from myapp.models import Note
 
 core = Blueprint('core', __name__)
 
 @core.route('/')
 def index():
-    
-    return render_template('index.html')
+    page = request.args.get('page', 1, type=int)
+    notes = Note.query.order_by(Note.date.desc()).paginate(page=page, per_page=5)
+    return render_template('index.html', notes=notes)
 
 @core.route('/info')
 def info():
